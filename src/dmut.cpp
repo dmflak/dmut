8a8764772ee9516cf9ad4c1ae21e7a80155de0ca
David McNulty <david.w.mcnulty@gmail.com>
2023-03-21 19:34:55 +0000

    Cleanup of main .cpp.

--- src/dmut.cpp
+++ src/dmut.cpp
@@ -11,38 +11,9 @@ namespace dmut::test
 void TestNonNullOwningPointer();
 
 } // namespace dmut::test
+
 int main()
 {
-
-    using HourOfDay = dmut::ConstrainedType<int, [](const int i)
-    {
-        return 0 <= i && i <= 23;
-    }>;
-
-    using NoughtToFiftyNine = dmut::ConstrainedType<int, [](const int i)
-    {
-        return 0 <= i && i <= 59;
-    }>;
-
-    using MinuteOfHour = NoughtToFiftyNine;
-    using SecondOfMinute = NoughtToFiftyNine;
-
-    struct TimeOfDay
-    {
-        HourOfDay hourOfDay;
-        MinuteOfHour minuteOfHour;
-        SecondOfMinute secondOfMinute;
-    };
-
-    auto bob1 = dmut::NonNullOwningPointer<std::unique_ptr<int>>::MakeOptional(
-        std::make_unique<int>());
-    auto bob2 = dmut::NonNullOwningPointer<std::unique_ptr<int>>::MakeOptional(
-        nullptr);
-    if (bob2.has_value())
-    {
-
-    }
-    //struct 
     dmut::test::TestNonNullOwningPointer();
     std::cout << "Done.\n";
     return 0;
